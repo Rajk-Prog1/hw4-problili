c63e8f846e5c33ad00c688c321bdc8b93718be2c
problili <probojcsevity.lili@gmail.com>
2024-10-30 17:15:05 +0100

    valami nem működik de nem értem mi

--- homework/minion_game/minion_game.py
+++ homework/minion_game/minion_game.py
@@ -1,3 +1,12 @@
 def minion_game(s: str) -> str:
-    """Write your code here"""
-    pass
+    vowels = 'AIEOU'
+    score_1 = len(sorted(s[i:j] for i, x in enumerate(s) for j in range(i + 1, len(s) + 1) if x in vowels))
+    score_2 = len(sorted(s[i:j] for i, x in enumerate(s) for j in range(i + 1, len(s) + 1) if x not in vowels))
+    if score_1 > score_2:
+        return f'Kevin {score_1}'
+    elif score_1 < score_2:
+         return f'Stuart {score_2}'
+    else:
+        return 'DRAW'
+
+print(minion_game('BANANA'))
